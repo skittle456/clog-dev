b5ab5a78093931039699ed7f1f5bfb84d55771d2
Jethnithi <bagpigham@gmail.com>
2018-05-31 03:03:37 +0700

    blog created, init migration

--- .history/apis/urls_20180528032749.py
+++
@@ -1,6 +0,0 @@
-from apis import views
-from django.conf.urls import url
-
-urlpatterns = [
-    url(r'^signin',views.signin,name='signin'),
-]
--- apis/models.py
+++ apis/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    blog_id = models.AutoField(max_length=10,primary_key=True)
+    img_url = models.URLField(null=True,blank=True)
+    url = models.URLField(null=True,blank=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=70) 
+    created_on = models.DateTimeField(auto_now_add=True)
