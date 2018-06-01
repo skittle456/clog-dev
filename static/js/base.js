f93c92aaeabed39baa507d6eccfa44895fc33b75
Jethnithi <bagpigham@gmail.com>
2018-06-01 18:45:04 +0700

    static files uploaded

---
+++ static/js/base.js
@@ -0,0 +1,23 @@
+
+function post_feedback(){
+    console.log($('textarea#message-text.form-control').val());
+    console.log($('input#email.form-control').val());
+    var request = $.ajax({
+        url: "/apis/feedback_list",
+        method: "POST",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify({
+            "email":$('input#email.form-control').val(),
+            "feedback_message":$('textarea#message-text.form-control').val()
+        }),
+    }).done(function(){
+        $('input#email.form-control').val('');
+        $('textarea#message-text.form-control').val('');
+        $('button.close').click();
+        alert("Thank you for feedback");
+    })
+    .fail(function() {
+        alert("fail")
+    });
+}
