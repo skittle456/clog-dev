9219a55e4edaad8457eb19416c2a3e73b7aafcce
Jethnithi <bagpigham@gmail.com>
2018-06-01 18:43:51 +0700

    settings config

--- clog/urls.py
+++ clog/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
 from apis import views
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apis/',include('apis.urls')),
+    url(r'^favicon\.ico$', favicon_view),
 ]
