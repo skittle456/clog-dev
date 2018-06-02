83e79b9b41b58e8ee95c44a067b5645e111c4061
Jethnithi <bagpigham@gmail.com>
2018-06-02 20:16:19 +0700

    whitenose detected

--- clog/wsgi.py
+++ clog/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
@@ -15,4 +16,18 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clog.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+if 'runserver' in sys.argv:
+    from django.conf import settings
+    import inspect
+    if settings.DEBUG:
+        has_staticfiles = any(
+            "django/contrib/staticfiles/management/commands/runserver"
+            in x[1]
+            for x in inspect.stack())
+
+if has_staticfiles:
+    print('runserver with staticfiles detected, skipping whitenoise')
+else:
+    # Do not use whitenoise with runserver, as it's slow
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(application)
