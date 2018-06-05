f6d7e5f24a4c00f9d8c4c6a9b09d4eeecd93eb43
Jethnithi <bagpigham@gmail.com>
2018-06-05 16:37:08 +0700

    spinner, timesince format

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,7 @@
 jQuery(function($) {
     $("img.lazy").lazyload({
-    threshold: 50
+    threshold: 50,
+    data_attribute: "orig"
 });
 $(window).on('load', function(){
     $("html,body").trigger("scroll");
