0243cf87e3842cb57bd7495a6d496971dbff7846
Jethnithi <bagpigham@gmail.com>
2018-06-05 17:07:02 +0700

    lazyload removed

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,9 @@
-jQuery(function($) {
-    $("img.lazy").lazyload({
-    threshold: 50,
-    data_attribute: "orig"
-});
-$(window).on('load', function(){
-    $("html,body").trigger("scroll");
-})
-});
+// jQuery(function($) {
+//     $("img.lazy").lazyload({
+//     threshold: 50,
+//     data_attribute: "orig"
+// });
+// $(window).on('load', function(){
+//     $("html,body").trigger("scroll");
+// })
+// });
