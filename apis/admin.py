d8851b8119a63a8faf7503ac760352e1ac8ff1fa
Jethnithi <bagpigham@gmail.com>
2018-06-08 00:19:11 +0700

    django super admin

--- apis/admin.py
+++ apis/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from apis.models import *
 # Register your models here.
+admin.site.register(Blog)
+admin.site.register(Provider)
+admin.site.register(Category)
+admin.site.register(Feedback)
