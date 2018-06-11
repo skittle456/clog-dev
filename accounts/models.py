efb94205d31ea13983ad907d3df6f5070e2576d6
Jethnithi <bagpigham@gmail.com>
2018-06-11 19:27:11 +0700

    accounts app added

---
+++ accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from apis.models import Blog
+# Create your models here.
+
+class User(AbstractUser):
+    pin_blog = models.ManyToManyField(Blog)
+    class Meta:
+        db_table = 'auth_user'
