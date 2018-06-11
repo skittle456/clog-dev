c5c7ae2c2383203a1545273dc7ce38e3c2d37a71
Jethnithi <bagpigham@gmail.com>
2018-06-11 16:40:40 +0700

    change logo, pin icon added

--- apis/urls.py
+++ apis/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^blog_list',views.BlogList.as_view(),name='blog_list'),
     url(r'^provider_list',views.ProviderList.as_view(),name='provider_list'),
     url(r'^feedback_list',views.FeedbackList.as_view(),name='feedback_list'),
-    url(r'^pin/(?P<blog_id>[\w]+)$', views.Pin.as_view()),
+    url(r'^pin/(?P<blog_id>[0-9]+)$', views.Pin.as_view()),
 ]
