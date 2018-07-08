b8e333fc1d63b22d7f295935573d697adfdb8376
Jethnithi <bagpigham@gmail.com>
2018-07-08 10:49:42 +0700

    insource format

--- clog/urls.py
+++ clog/urls.py
@@ -33,5 +33,6 @@ urlpatterns = [
     url(r'^blog/(?P<blog_id>[\w-]+)/$', views.get_insource_blog, name='get_insource_blog'),
     url(r'^clog',views.editor,name='editor'),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
