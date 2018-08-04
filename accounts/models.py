67cb8cdb69c2abedcfe8d39a24b01036e5261ca8
Jethnithi <bagpigham@gmail.com>
2018-08-05 05:23:27 +0700

    like_blog field added

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from apis.models import Blog, Provider
+from apis.models import Blog, Provider, Insource
 # Create your models here.
 
 class User(AbstractUser):
     pin_blog = models.ManyToManyField(Blog)
     follow_provider = models.ManyToManyField(Provider)
+    like_blog = models.ManyToManyField(Insource)
     #watched_blog = models.ManyToManyField(Blog)
     class Meta:
         db_table = 'auth_user'
