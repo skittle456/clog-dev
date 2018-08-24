aae2d10b1012964128af7fe106d91769c9784683
Charnchyyy <dhitiwat.jc@gmail.com>
2018-08-24 20:45:15 +0100

    Update 7 days trending

--- apis/recommender.py
+++ apis/recommender.py
@@ -3,22 +3,29 @@ import datetime
 
 class Recommender:
     
-    def __init__():
+    def __init__(self):
         pass
     
     ## Get most view blog in the last 7 days
     ## size of return blog =  number_of_content (default = 7)
     ## TODO: Check if this actually work
-    def getTrendingFromLast7Days(number_of_content = 7):
+    def getTrendingInTheLast7days(self,number_of_content = 7):
         now = datetime.datetime.now()
         blog_list = Blog.objects.order_by('-total_views')
+        if len(blog_list) == 0:
+            return []
+        tz_info = blog_list[0].created_on.tzinfo
+        now = datetime.datetime.now(tz_info)
         trending_list = []
+        count = 0
         for blog in blog_list:
             if len(trending_list) == number_of_content:
                 break
+                
             time_difference = now - blog.created_on
-            if abs(time_difference.day) < 7:
+            if abs(time_difference.days) < 7:
                 trending_list.append(blog)
+
         return trending_list
 
     
