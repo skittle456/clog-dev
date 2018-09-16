9ebf0160dc98415c1ac3f67f566fab2a1cac7be9
Jethnithi <bagpigham@gmail.com>
2018-09-17 03:57:02 +0700

    user on WriterRegistration field added

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,11 @@ class User(AbstractUser):
     is_writer = models.BooleanField(default=False)
     writer = models.ForeignKey(Provider, on_delete=models.CASCADE,related_name='writer',null=True,blank=True,default=None)
     #watched_blog = models.ManyToManyField(Blog)
+    # GENDER_CHOICES = (
+    #     ('M', 'Male'),
+    #     ('F', 'Female'),
+    # )
+    # gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     class Meta:
         db_table = 'auth_user'
 
@@ -20,6 +25,7 @@ class WriterRegistration(models.Model):
         ('declined','Declined'),
     )
     request_id = models.AutoField(max_length=10,primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     description = models.TextField()
     status = models.CharField(choices=status_choices,max_length=30,default='held_for_review')
     created_on = models.DateTimeField(auto_now_add=True)
