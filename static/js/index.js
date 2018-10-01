a783d45ea55fffb538945add6caaab693a2bebf8
Jethnithi <bagpigham@gmail.com>
2018-10-01 15:54:03 +0700

    crop related post, remove carousel control, change bloglink and date post position, responsive

--- static/js/index.js
+++ static/js/index.js
@@ -9,7 +9,7 @@ var blogs = null;
 $('div#demo').on('slide.bs.carousel', function (e) {
         var $e = $(e.relatedTarget);
         var idx = $e.index();
-        var itemsPerSlide = 4;
+        var itemsPerSlide = 1;
         var totalItems = $('.carousel-item').length;
         
         if (idx >= totalItems-(itemsPerSlide-1)) {
